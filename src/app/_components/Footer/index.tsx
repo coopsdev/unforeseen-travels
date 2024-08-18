24296e45a7227070caefe58d7186b0e8a7ceb4ad
Cooper Larson <cooper.larson1@gmail.com>
2024-08-18 13:54:38 -0600

    Cleanup footer and add CI/CD workflow

--- src/app/_components/Footer/index.tsx
+++ src/app/_components/Footer/index.tsx
@@ -30,8 +30,8 @@ export async function Footer() {
           <picture>
             <img
               className={classes.logo}
-              alt="Payload Logo"
-              src="https://raw.githubusercontent.com/payloadcms/payload/main/packages/payload/src/admin/assets/images/payload-logo-light.svg"
+              alt="Cooper Larson's Logo"
+              src="https://res.cloudinary.com/ddddyraui/image/upload/cl-logo_e5nuxu.png"
             />
           </picture>
         </Link>
@@ -40,17 +40,6 @@ export async function Footer() {
           {navItems.map(({ link }, i) => {
             return <CMSLink key={i} {...link} />
           })}
-          <Link href="/admin">Admin</Link>
-          <Link
-            href="https://github.com/payloadcms/payload/tree/main/templates/website"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Source Code
-          </Link>
-          <Link href="https://payloadcms.com" target="_blank" rel="noopener noreferrer">
-            Payload
-          </Link>
         </nav>
       </Gutter>
     </footer>
