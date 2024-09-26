91c6a1c345f2ad331b3ea110e4eb3b5b5ff1f748
Cooper Larson <cooper.larson1@gmail.com>
2024-09-26 14:56:29 -0600

    rm last babylon refs

--- src/app/_components/Media/index.tsx
+++ src/app/_components/Media/index.tsx
@@ -1,6 +1,5 @@
 import React, { Fragment } from 'react'
 
-import { BabylonScene } from './BabylonScene'
 import { Image } from './Image'
 import { Props } from './types'
 import { Video } from './Video'
@@ -13,15 +12,11 @@ export const Media: React.FC<Props> = props => {
 
   const isVideo = (): boolean => isMedia(resource) && resource.mimeType?.includes('video')
   const isImage = (): boolean => isMedia(resource) && resource.mimeType?.includes('image')
-  const isModel = (): boolean =>
-    isMedia(resource) &&
-    (resource.mimeType?.includes('model') || resource.mimeType?.includes('octet-stream'))
 
   const Tag = (htmlElement as any) || Fragment
 
   const MediaRenderer = () => {
     if (isImage()) return <Image {...props} />
-    else if (isModel()) return <BabylonScene {...props} />
     else if (isVideo()) return <Video {...props} />
     return (
       <div>
