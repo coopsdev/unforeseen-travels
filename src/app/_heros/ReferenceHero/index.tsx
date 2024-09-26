eb6d527f29d270eddcc149abd7d46b320029400d
Cooper Larson <cooper.larson1@gmail.com>
2024-09-26 09:24:08 -0600

    image styling

--- src/app/_heros/ReferenceHero/index.tsx
+++ src/app/_heros/ReferenceHero/index.tsx
@@ -1,8 +1,7 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import { Rating } from 'react-simple-star-rating'
 
 import { Reference } from '../../../payload/payload-types'
-import { Gutter } from '../../_components/Gutter'
 import { Media } from '../../_components/Media'
 import RichText from '../../_components/RichText'
 
@@ -13,35 +12,62 @@ export const ReferenceHero: React.FC<{
 }> = ({ reference }) => {
   const { id, title, location, review, media, rating } = reference
 
-  return (
-    <div className={classes.content}>
+  const InfoBar = () => (
+    <div className={classes.infoBar}>
+      <h5 className={classes.location}>{location}</h5>
+      <Rating
+        className={classes.rating}
+        initialValue={rating}
+        fillColor="gold"
+        emptyColor="lightgray"
+        allowFraction
+        allowHover={false}
+        size={25}
+      />
+    </div>
+  )
+
+  const Meta = () => (
+    <>
       <h1 className={classes.title}>{title}</h1>
-      <div className={classes.infoBar}>
-        <h5 className={classes.location}>{location}</h5>
-        <Rating
-          className={classes.rating}
-          initialValue={rating}
-          fillColor="gold"
-          emptyColor="lightgray"
-          allowFraction
-          allowHover={false}
-          size={25}
-        />
-      </div>
-      <div className={classes.body}>
-        <p className={classes.review}>{review}</p>
-        <div className={classes.media}>
-          <div className={classes.mediaWrapper}>
-            {!media && <div className={classes.placeholder}>No image</div>}
-            {media && typeof media !== 'string' && (
-              <Media imgClassName={classes.image} resource={media} fill />
-            )}
-          </div>
-          {media && typeof media !== 'string' && typeof media !== 'number' && media?.caption && (
-            <RichText content={media.caption} className={classes.caption} />
+      <InfoBar />
+    </>
+  )
+
+  const MediaComponent = () => {
+    if (!media || typeof media === 'number') return <div>Media is undefined</div>
+    const isLandscape = media?.width > media?.height
+
+    return (
+      <div className={classes.media}>
+        <div className={classes.mediaWrapper}>
+          {!media && <div className={classes.placeholder}>No image</div>}
+          {media && typeof media !== 'string' && (
+            <Media
+              imgClassName={`${classes.image} ${isLandscape ? classes.landscape : ''}`}
+              resource={media}
+              fill
+            />
           )}
         </div>
+        {media && typeof media !== 'string' && typeof media !== 'number' && media?.caption && (
+          <RichText content={media.caption} className={classes.caption} />
+        )}
       </div>
+    )
+  }
+
+  const Body = () => (
+    <div className={classes.body}>
+      <p className={classes.review}>{review}</p>
+      <MediaComponent />
+    </div>
+  )
+
+  return (
+    <div className={classes.content}>
+      <Meta />
+      <Body />
     </div>
   )
 }
